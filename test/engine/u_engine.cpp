5f94e448ee1a54d8caf59bc50a41e174cb039216
Robert Ikanov <robert.ikanov@gmail.com>
2021-05-09 11:01:41 +0200

    [add] sufficient test cases

--- test/engine/u_engine.cpp
+++ test/engine/u_engine.cpp
@@ -13,21 +13,22 @@ bool UnitTests::unit_Engine() const
                   CPositions<3>::GetPosID( 0, 1, 2 ),
                   CPositions<3>::GetPosID( 2, 1, 2 ),
                   CPositions<3>::GetPosID( 1, 2, 2 ),
-           //       CPositions<3>::GetPosID( 2, 2, 2 )
+                  CPositions<3>::GetPosID( 1, 2, 1 )
+  };
+  SubSpace cornerBlock = {
+                  CPositions<3>::GetPosID( 0, 0, 1 ),
+                  CPositions<3>::GetPosID( 0, 0, 2 ),
+                  CPositions<3>::GetPosID( 0, 2, 1 ),
+                  CPositions<3>::GetPosID( 0, 2, 2 ),
+                  CPositions<3>::GetPosID( 0, 1, 2 ),
   };
   SubSpace corners = {
                   CPositions<3>::GetPosID( 0, 0, 2 ),
                   CPositions<3>::GetPosID( 0, 2, 2 ),
                   CPositions<3>::GetPosID( 2, 0, 2 ),
                   CPositions<3>::GetPosID( 2, 2, 2 ),
-                  CPositions<3>::GetPosID( 1, 1, 2 ),
-                  CPositions<3>::GetPosID( 1, 1, 2 )
-  };
-  SubSpace cornerBlock = {
-                  CPositions<3>::GetPosID( 0, 0, 1 ),
-                  CPositions<3>::GetPosID( 0, 1, 1 ),
-                  CPositions<3>::GetPosID( 0, 0, 2 ),
-                  CPositions<3>::GetPosID( 0, 1, 2 ),
+                  CPositions<3>::GetPosID( 2, 2, 1 )
+
   };
   SubSpace edgePool = {
                   CPositions<3>::GetPosID( 0, 0, 1 ),
@@ -63,14 +64,10 @@ bool UnitTests::unit_Engine() const
   Insight<3> step_1 ( cross );
   Insight<3> test   ( cornerBlock );
   Insight<3> step_2 ( test, 0 );
-  Insight<3> step_3 ( test, Simplex::Tilt( _Z, 1 ) );
-  Insight<3> step_4 ( test, Simplex::Tilt( _Z, 2 ) );
-  Insight<3> step_5 ( test, Simplex::Tilt( _Z, 3 ) );
- // Insight<3> addCache ( edgePool );
- /* Insight<3> step_6 ( cross,   Simplex::Tilt( _X, 2 ),  _Z );
-  Insight<3> step_7 ( corners, Simplex::Tilt( _X, 2 ),  _Z );
-  Insight<3> step_8 ( lastEdges );
-  Insight<3> step_9 ( lastCorners );*/
+  Insight<3> step_3 ( test, Simplex::Tilt( _Z, 2 ) );
+  Insight<3> step_6 ( step_1,   Simplex::Tilt( _X, 2 ) );
+  Insight<3> step_7 ( corners,  Simplex::Tilt( _X, 2 ), _Z );
+  Insight<3> step_8 ( corners,  Simplex::Tilt( _X, 2 ) );
   timerOFF();
   clog( Color::cyan, "Ellapsed time:", Color::white, Color::bold, ellapsed(), Color::off );
 
@@ -83,19 +80,23 @@ bool UnitTests::unit_Engine() const
   Engine<3>  testEngine1( testCube3 );
   Engine<3>  testEngine2( testCube3 );
   testEngine2 << step_1;
-  testEngine2 << step_2;
   testEngine2.run( 11 );
+  testCube3.print();
+  testEngine2 << step_2;
+  testEngine2.run( 12 );
   testCube3.print();
   testEngine2 << step_3;
-  testEngine2.run( 11 );
+  testEngine2.run( 12 );
   testCube3.print();
-  testEngine2 << step_4;
-  testEngine2.run( 11 );
+  testEngine2 << step_6;
+  testEngine2.run( 12 );
   testCube3.print();
-  testEngine2 << step_5;
-  testEngine2.run( 11 );
+  testEngine2 << step_7;
+  testEngine2.run( 12 );
   testCube3.print();
-
+  testEngine2 << step_8;
+  testEngine2.run( 12 );
+  testCube3.print();
  // testEngine2 << addCache;
  // testEngine2.run( 11 ); // it does nothing right now
  //  testCube3.print();
