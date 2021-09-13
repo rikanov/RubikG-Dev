b261b5ae8f0af5b150336d00ac16dc7d48ad3e1a
Róbert Ikanov <robert.ikanov@ericsson.com>
2021-09-13 13:47:38 +0200

    [add] unit tests for Projection
    Change-Id: If1d2918240f90a2ab2736d1c51cd06dc038ffb2e

--- test/core/u_projection.cpp
+++ test/core/u_projection.cpp
@@ -1,11 +1,40 @@
 #include <test.h>
+#include <simplex.h>
 #include <projection.h>
+
+static void RandomGroupID( GroupID & normal, GroupID & projected, size_t size )
+{  
+  const CubeID prior = Simplex::Random();
+  normal    = prior * pow24( -- size );
+  projected = 0;
+  while( 0 < size -- )
+  {
+    const CubeID random = Simplex::Random();
+    normal    += Simplex::Composition( random, prior ) * pow24( size );
+    projected += random * pow24( size );
+  }
+}
 
 bool UnitTests::unit_Projection() const
 {
   bool success = true;
   head( "Subgroup projections" );
 
+  for ( size_t size = 2; size < 6; ++ size )
+  {
+    bool ok = true;
+    GroupID normal, projected;
+    
+    tcase( "Projection", "size", size );
+    for ( int i = 0; i < 5; ++ i )
+    {
+      RandomGroupID( normal, projected, size );
+      clog_( Color::cyan, normal, Color::yellow, "\t-->", Color::cyan, projected, '\t' );
+      stamp( Projection::LookUp( size, normal ) == projected, ok );
+    }
+    success &= ok;
+    tail( "Projection", success );
+  }
   
   finish( "Subgroup projections", success );
   return success;
